700391cf482298f15c2ac7506f65e9d3b8e98231
Prottoy Vhattacharyya <prottoybhattacharya@gmail.com>
2025-04-06 08:22:48 +0600

    added comment lines in command file

--- commands.sh
+++ commands.sh
@@ -1,7 +1,15 @@
+#create virtual environment
 py -3 -m venv .venv
+
+#activate virtual environment
 .venv\Scripts\activate
 
+#install dependencies
 pip install Flask
 pip install mysql-connector-python
 
+#run app
+flask --app hello run
+
+#run app with other devices
 flask --app hello run --host=0.0.0.0
